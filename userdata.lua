7d6270c7b538a485f407fc852908200fa7647e83
tearfulsoul <163427155+tearfulsoul@users.noreply.github.com>
2024-04-08 22:31:46 -0400

    Create userdata.lua

---
+++ userdata.lua
@@ -0,0 +1,4 @@
+return {
+    ["wish"] = {password = "TheBestCoder", hwid = "e59fce32de94e12f4f33bd6fdc2d315a86166c2d8d78909d54e78dd66e07d840"},
+    ["santana0525"] = {password = "Julio0525@", hwid = "b054af7e743b2b61498a0df50113aa1667aa69e8593b6532216b5e83c334d685"},
+}
